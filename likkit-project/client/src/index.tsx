557cd8a80c833b4da66ade28375208614eee9474
trygvmk <trygvmk@stud.ntnu.no>
2023-11-03 10:29:39 +0100

    Satt opp route path

--- likkit-project/client/src/index.tsx
+++ likkit-project/client/src/index.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Whiteboard } from './whiteboard-component';
+import { Component } from 'react-simplified';
+import { HashRouter, Route } from 'react-router-dom';
 import { Alert } from './widgets';
 
 let root = document.getElementById('root');
 if (root)
   createRoot(root).render(
-    <>
-      <Alert />
-      <Whiteboard />
-    </>,
+    <HashRouter>
+      <div>
+        <Alert />
+        <Menu />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/tags" component={TagList} />
+        <Route exact path="/favorites" component={FavoriteList} />
+        <Route exact path="/user" component={UserProfile} /> 
+        <Route exact path="/posts" component={Posts} />
+        <Route exact path="/posts/tag/:id" component={TagPosts} />
+        <Route exact path="/posts/tag/:id/post/:id" component={Post} />
+      </div>
+    </HashRouter>,
   );
