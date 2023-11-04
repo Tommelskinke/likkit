5b5eee20273122b65a3f24c8b01aa2abc105bc91
trygvmk <trygvmk@stud.ntnu.no>
2023-11-04 19:10:01 +0100

    Videre jobbing med main page

--- likkit-project/client/src/Components/whiteboard-component.tsx
+++ likkit-project/client/src/Components/whiteboard-component.tsx
@@ -70,6 +70,7 @@ export class Whiteboard extends Component {
 }
 
 export class Home extends Component {
+  search: string = '';
   render() {
     return (
       <div>
@@ -81,19 +82,34 @@ export class Home extends Component {
                 alt="Picture of likkit logo"
               />
             </Column>
-            <Column width={8}>
+            <Column width={8} none>
               <Form.Input
                 type="text"
-                //value={this.task.title}
-                //onChange={(event) => (this.task.title = event.currentTarget.value)}
+                value={this.search}
+                onChange={(event) => (this.search = event.currentTarget.value)}
                 placeholder="Søk..."
               />
             </Column>
-            <Column width={1}>
+            <Column width={1} right>
               <img
                 src="https://cdn.discordapp.com/attachments/623523695540830219/1169948601183649832/380254333_641845358065071_8017670276526516197_n.png?ex=6557428b&is=6544cd8b&hm=45e12c97e4c20ea17fc19d8feb50b18def1fa2ad524236098cb95bba40b4a144&"
                 alt="Picture of likkit logo"
               />
+            </Column>
+          </Row>
+        </Card>
+        <Card>
+          <Row>
+            <Column width={1}></Column>
+            <Column width={8} none>
+              <div id="createPost" onClick={() => console.log('alooo')}>
+                <Form.Input
+                  type="text"
+                  value={this.search}
+                  onChange={(event) => (this.search = event.currentTarget.value)}
+                  placeholder="Create a post..."
+                />
+              </div>
             </Column>
           </Row>
         </Card>
