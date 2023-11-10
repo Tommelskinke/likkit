59f6b334f4ff63b5421d103a4a90127e0fb77ace
trygvmk <trygvmk@stud.ntnu.no>
2023-11-10 11:45:11 +0100

    Prettier ting

--- likkit-project/client/src/Components/userpage-components.tsx
+++ likkit-project/client/src/Components/userpage-components.tsx
@@ -13,39 +13,28 @@ export class UserProfile extends Component {
   render() {
     return (
       <>
-      <Card title='User profile'>
-        <Column width={3}>
-          <Row >
-            Username: {this.user}
-          </Row>
-          <Row>
-            Usertype: {this.userType}
-          </Row>
-        </Column>
-        <Column width={3}>
-          <Row>
-            Licks: {this.likkAmount}
-          </Row>
-          <Row>
-            Upvotes: {this.upvoteAmount}
-          </Row>
-        </Column>
-        <Column right width={3}>
-          <Row>
-            Comments: {this.commentAmount}
-          </Row>
-          <Row>
-            Best comments: {this.bestCommentAmount}
-          </Row>
-        </Column>
-      <Button.Light>Favorite Posts</Button.Light>
-      <Button.Light>Best Post</Button.Light>
-      <Button.Light>Best Comment</Button.Light>
-      <Button.Light>Favorite Comments</Button.Light>
-      <Button.Light>All Posts</Button.Light>
-      <Button.Light>All Comments</Button.Light>
-      </Card>
+        <Card title="User profile">
+          <Column width={3}>
+            <Row>Username: {this.user}</Row>
+            <Row>Usertype: {this.userType}</Row>
+          </Column>
+          <Column width={3}>
+            <Row>Licks: {this.likkAmount}</Row>
+            <Row>Upvotes: {this.upvoteAmount}</Row>
+          </Column>
+          <Column right width={3}>
+            <Row>Comments: {this.commentAmount}</Row>
+            <Row>Best comments: {this.bestCommentAmount}</Row>
+          </Column>
+          <Button.Light>Favorite Posts</Button.Light>
+          <Button.Light>Best Post</Button.Light>
+          <Button.Light>Best Comment</Button.Light>
+          <Button.Light>Favorite Comments</Button.Light>
+          <Button.Light>All Posts</Button.Light>
+          <Button.Light>All Comments</Button.Light>
+        </Card>
       </>
     );
   }
 }
+// må bare ha en endring for prettier
