2d263b674cd611bd1d7401c219ad64f65aa4c27c
Oscar Bergholtz <Oscarbe@stud.ntnu.no>
2023-11-10 10:46:25 +0100

    lagt til nødvendige filer for å bruke databasen

---
+++ likkit-project/server/src/task-service.ts
@@ -0,0 +1,33 @@
+import pool from './mysql-pool';
+import type { RowDataPacket, ResultSetHeader } from 'mysql2';
+
+export type Question = {
+  question_id: number;
+  user_id: number;
+  tag_id: [];
+  title: string;
+  content: string;
+  created_at: Date;
+  upvotes: number;
+  downvotes: number;
+  karma: number;
+};
+
+class TaskService {
+  /**
+   * Get question with given id.
+   */
+  get(question_id: number) {
+    return new Promise<Question | undefined>((resolve, reject) => {
+      pool.query('SELECT * FROM question WHERE id = ?', [question_id], (error, results: RowDataPacket[]) => {
+        if (error) return reject(error);
+
+        resolve(results[0] as Question);
+      });
+    });
+  }
+
+}
+
+const taskService = new TaskService();
+export default taskService;
