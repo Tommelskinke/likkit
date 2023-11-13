3045d8e273842f30d57d29bb311c528c253278c2
Vegard Torkildsen Udnes <vegardudnes@outlook.com>
2023-11-13 14:17:36 +0100

    autocomplete i search bar, mangler å testes

--- likkit-project/client/src/Components/search-component.tsx
+++ likkit-project/client/src/Components/search-component.tsx
@@ -1,24 +1,71 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
+import debounce from 'lodash.debounce';
 import searchService from '../search-service';
 
 function SearchBar(): JSX.Element {
-  const [search, setSearch] = useState<string>('');
+  const [input, setInput] = useState<string>('');
+  const [suggestions, setSuggestions] = useState<Array<any>>([]);
+  const [searchResults, setSearchResults] = useState<Array<any>>([]); // State to hold search results
+
+  const fetchSuggestions = debounce(async (query) => {
+    const response = await axios.get(`http://localhost:3000/api/suggest?term=${query}`);
+    setSuggestions(response.data || []);
+  }, 300);
+
+  useEffect(() => {
+    if (input) fetchSuggestions(input);
+  }, [input]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
+    setInput(event.target.value);
+    fetchSuggestions(event.target.value);
   };
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(search);
-    searchService.searchQuestions(search);
-    setSearch('');
+    try {
+      const results = await searchService.searchQuestions(input);
+      setSearchResults(results); // Update the state with the search results
+      setInput(''); // Reset the input field after search
+      setSuggestions([]); // Clear suggestions after search
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+      // Handle error (e.g., show an error message)
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={search} onChange={handleInputChange} placeholder="Search..." />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" value={input} onChange={handleInputChange} placeholder="Search..." />
+          {suggestions.length > 0 && (
+            <ul>
+              {suggestions.map((suggestion, index) => (
+                <li key={index} onClick={() => setInput(suggestion.title)}>
+                  {suggestion.title}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </form>
+      <div>
+        {searchResults.length > 0 && (
+          <div>
+            <h2>Search Results</h2>
+            <ul>
+              {searchResults.map((result, index) => (
+                // Display the search results here
+                // Adjust the rendering based on your result object structure
+                <li key={index}>{result.title}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
--- likkit-project/server/src/search-router.ts
+++ likkit-project/server/src/search-router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import searchService from './search-service';
+import pool from './mysql-pool';
 
 /**
  * Express router containing task methods.
@@ -15,6 +16,25 @@ router.get('/posts', async (request, response) => {
     } else {
       response.status(400).json({ error: 'Invalid search query.' });
     }
+  } catch (error) {
+    response.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.get('/api/suggest', async (request, response) => {
+  try {
+    const partialQuery = request.query.term; // Assume 'term' is the query parameter
+    if (!partialQuery) {
+      return response.status(400).json({ error: 'No search term provided' });
+    }
+
+    // Example query, adjust according to your data schema
+    const suggestions = await pool.query(
+      'SELECT DISTINCT title FROM question WHERE title LIKE CONCAT("%", ?, "%") LIMIT 10',
+      [partialQuery],
+    );
+
+    response.json(suggestions);
   } catch (error) {
     response.status(500).json({ error: 'Internal server error' });
   }
