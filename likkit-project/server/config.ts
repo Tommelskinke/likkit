b22e052f690dc29a52d966e4a004ea88e6f738dc
trygvmk <126457341+trygvmk@users.noreply.github.com>
2023-11-19 17:42:23 +0100

    Create config.ts
    CI test

---
+++ likkit-project/server/config.ts
@@ -0,0 +1,5 @@
+const crypto = require('crypto');
+
+export const SESSION_SECRET = process.env.SESSION_SECRET || '';
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '';
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || '';
