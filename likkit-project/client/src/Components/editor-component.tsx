32f1553b31185da6333b9051d99031efa16371c6
Gnibs <37116829+Gnibs@users.noreply.github.com>
2023-11-20 22:04:08 +0100

    Innhold vises når man redigerer

--- likkit-project/client/src/Components/editor-component.tsx
+++ likkit-project/client/src/Components/editor-component.tsx
@@ -7,18 +7,25 @@ import 'react-quill/dist/quill.snow.css';
 // Define the props expected by EditorComponent
 interface EditorComponentProps {
   onContentChange: (content: string) => void;
+  initialValue?: string;
 }
 
 // Use the props in your component
 class EditorComponent extends React.Component<EditorComponentProps> {
   state = {
-    editorHtml: '',
+    editorHtml: this.props.initialValue || '',
   };
 
   handleChange = (html: string) => {
     this.setState({ editorHtml: html });
     this.props.onContentChange(html); // Call the passed onContentChange function
   };
+
+  componentDidUpdate(prevProps: EditorComponentProps) {
+    if (prevProps.initialValue !== this.props.initialValue) {
+      this.setState({ editorHtml: this.props.initialValue || '' });
+    }
+  }
 
   render() {
     return <ReactQuill value={this.state.editorHtml} onChange={this.handleChange} />;
